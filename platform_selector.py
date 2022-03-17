571f1819bcd97ae0af5b54177fedb0330400dafe
eugeneslastin <e.slastin@gmail.com>
2022-03-17 12:57:18 +0300

    other platforms added

--- platform_selector.py
+++ platform_selector.py
@@ -1,6 +1,12 @@
+from parsers.akson import ParserAkson
 from parsers.baucenter import ParserBau
 from parsers.dns import ParserDns
+from parsers.eltsnab import ParserEltsnab
+from parsers.etm import ParserEtm
 from parsers.maxidom import ParserMaxidom
+from parsers.sdvor import ParserSdvor
+from parsers.toledo24 import ParserToledo24
+from parsers.votonia import ParserVotonia
 
 
 def select_platform(platform):
@@ -12,16 +18,16 @@ def select_platform(platform):
         case 'baucenter':
             return ParserBau
         case 'akson':
-            return None
+            return ParserAkson
         case 'etm':
-            return None
+            return ParserEtm
         case 'sdvor':
-            return None
+            return ParserSdvor
         case 'eltsnab':
-            return None
+            return ParserEltsnab
         case 'toledo24':
-            return None
+            return ParserToledo24
         case 'votonia':
-            return None
+            return ParserVotonia
         case _:
             print('not found platform in list')
